2ce28dc8239d79b76ef737d13abb2f053716459b
Francisco Gabriel <franciscogabriel.am@gmail.com>
2022-09-13 16:18:04 -0400

    [Wip] refatoring design menu dropdown

--- js/kickstart.js
+++ js/kickstart.js
@@ -463,7 +463,7 @@ function toggleSidebar() {
 
 /* When the user clicks on the button,
 		toggle between hiding and showing the dropdown content */
-		function myFunction() {
+		function myFunctionDropdown() {
 			document.getElementById("myDropdown").classList.toggle("show");
 		  }
 		  
